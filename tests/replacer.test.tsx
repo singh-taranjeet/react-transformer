312be8ff96d9c9c9aea7e6466d60258ee1b92cf6
singh-taranjeet <taranjeet.singh3009@gmail.com>
2023-04-05 23:10:48 +0530

    Added github actions

--- tests/replacer.test.tsx
+++ tests/replacer.test.tsx
@@ -7,11 +7,15 @@ import { Replacer } from '../src'
 import { Button } from './components/Button'
 import { Bold } from './components/Bold'
 
+const PREFIX = '<<'
+const SUFFIX = '>>'
+const SEPERATOR = '|'
+
 const config = {
   pattern: {
-    prefix: '<<',
-    suffix: '>>',
-    seperator: '|',
+    prefix: PREFIX,
+    suffix: SUFFIX,
+    seperator: SEPERATOR,
   },
   elementTypes: {
     button: Button,
@@ -27,10 +31,16 @@ describe('Renders correctly', () => {
       </Replacer>,
     )
   })
-  it('replaces pattern with components', () => {
+  it('replaces single pattern with components', () => {
     render(
       <Replacer config={config}>
-        <div data-testid={'test'}>corr{`<<|bold|${JSON.stringify({ data: { text: 'ect and wor' } })}|>>`}king</div>
+        <div data-testid={'test'}>
+          corr
+          {`${PREFIX}${SEPERATOR}bold${SEPERATOR}${JSON.stringify({
+            data: { text: 'ect and wor' },
+          })}${SEPERATOR}${SUFFIX}`}
+          king
+        </div>
       </Replacer>,
     )
     const element = screen.getByTestId('test')
