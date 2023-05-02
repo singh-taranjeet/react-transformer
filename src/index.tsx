ec430d07f1986c90d14cd7ce664072ef0543a838
singh-taranjeet <93113908+singh-taranjeet@users.noreply.github.com>
2023-05-02 22:19:43 +0530

    Dev (#10)
    * configured npm publish
    
    * Added example project
    
    * deleted DS_Store file
    
    * Added tests
    
    * Added github actions
    
    * Test commit
    
    * Code-refactor
    
    * updated read me file
    
    * updated ackage
    
    * Updated gitignor
    
    * Updated readme
    
    * updated git ignore
    
    * Handled invalid json
    
    * fixed linting errors
    
    * Fixed linting issues
    
    * moved typescript to dev-dependency
    
    * added-multiple-react-versions-for-testing
    
    * multiple react support
    
    * Update package.json
    
    * saving
    
    * created class based component
    
    * fixed bug
    
    * Update package.json
    
    * Update index.tsx
    
    ---------
    
    Co-authored-by: Taranjeet Singh <taranjeet.singh@telekom-digital.com>

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import ReactDom from 'react-dom'
-import ReactDOMClient from 'react-dom/client'
 import { IReactTransformer, IPattern } from './types'
 import { getId, getPatterns } from './utils'
 
