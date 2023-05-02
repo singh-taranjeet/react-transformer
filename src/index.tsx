fd449c824882e5f9b7d5288d9ed92a1b85652027
Taranjeet Singh <taranjeet.singh3009@gmail.com>
2023-05-02 22:53:25 +0530

    saving

--- src/index.tsx
+++ src/index.tsx
@@ -1,42 +1,19 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import { Replacer } from "@react-transformer/replacer";
-
-interface IComponent {
-  data: {
-    text: string;
-  };
-}
-
-const Button = (props: IComponent) => {
-  const { text } = props.data;
-
-  return <button>{text}</button>;
-};
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(
-  document.getElementById("root") as HTMLElement
+  document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <Replacer
-      config={{
-        pattern: {
-          prefix: "<<",
-          suffix: ">>>",
-          seperator: "|",
-        },
-        elementTypes: {
-          button: Button,
-        },
-      }}
-    >
-      <h1>Test</h1>
-      <div>
-        <p>{`<<|button|${JSON.stringify({
-          data: { text: "Test Button" },
-        })}|>>>`}</p>
-      </div>
-    </Replacer>
+    <App />
   </React.StrictMode>
 );
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
