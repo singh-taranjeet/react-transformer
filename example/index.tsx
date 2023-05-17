f260ae9bcc6f67f2c4757438694ae864d6b9fc74
singh-taranjeet <93113908+singh-taranjeet@users.noreply.github.com>
2023-05-17 11:38:15 +0530

    Fixed example

--- example/index.tsx
+++ example/index.tsx
@@ -1,8 +1,8 @@
-import 'react-app-polyfill/ie11';
-import * as React from 'react';
-import * as ReactDOM from 'react-dom';
-import { Replacer } from '../.';
-import { useState } from 'react';
+import 'react-app-polyfill/ie11'
+import * as React from 'react'
+import * as ReactDOM from 'react-dom'
+import { Replacer } from '../.'
+import { useState } from 'react'
 
 interface IComponent {
   data: {
@@ -20,14 +20,14 @@ const Bold = (props: IComponent) => {
   return <strong>{text}</strong>
 }
 
-const Strong = () => {
+const RandomConcat = () => {
   const [str, setStr] = useState(
-    `${PREFIX}${SEPERATOR}${ELEMENT_TYPE.BUTTON}${SEPERATOR}'{"data":{"text":"Strong Text"}}'${SEPERATOR}${SUFFIX}`,
+    `corr${PREFIX}${SEPERATOR}${ELEMENT_TYPE.BUTTON}${SEPERATOR}${JSON}${SEPERATOR}${SUFFIX}king`,
   )
   const onClick = () => {
-    setStr(`${Math.random()} added another=> ${str} pl`)
+    setStr(`${Math.random()} ((*)) ${str} ((*))`)
   }
-  return <strong onClick={onClick}>Strong {str}</strong>
+  return <strong onClick={onClick}>{str}</strong>
 }
 
 const PREFIX = '<<'
@@ -39,11 +39,9 @@ enum ELEMENT_TYPE {
   BUTTON = 'button',
   BOLD = 'bold',
 }
-const Temp = () => {
+const ShowHideString = () => {
   const [show, setShow] = React.useState(true)
-  const sep = '===='
   const onClick = () => {
-    //setStr(`${sep}${str}${sep}`);
     setInterval(() => {
       setShow(!show)
     }, 10)
@@ -51,18 +49,17 @@ const Temp = () => {
 
   return (
     <div>
-      dsf
       {show ? null : (
         <p data-testid={'paragraph'}>
-          corr{`${PREFIX}${SEPERATOR}bold${SEPERATOR}${JSON}${SEPERATOR}${SUFFIX}`}king Taranjeet Singh
+          corr{`${PREFIX}${SEPERATOR}bold${SEPERATOR}${JSON}${SEPERATOR}${SUFFIX}`}king
         </p>
       )}
-      <h1 onClick={onClick}>Taranjeet</h1>
+      <button onClick={onClick}>show/hide</button>
     </div>
   )
 }
 
-const Temp2 = () => {
+const StringManagedByState = () => {
   const [str, setStr] = React.useState(`<<!bold!'{"data":{"text":"ect and wor"}}'!>>king`)
   const onClick = () => {
     setInterval(() => {
@@ -81,45 +78,47 @@ const App = () => {
   return (
     <div>
       <Replacer
-      config={{
-        pattern: {
-          prefix: PREFIX,
-          suffix: SUFFIX,
-          seperator: SEPERATOR,
-        },
-        elementTypes: {
-          [ELEMENT_TYPE.BUTTON]: Button,
-          [ELEMENT_TYPE.BOLD]: Bold,
-        },
-      }}
-    >
-      <h1>Test#1</h1>
-      <div>
+        config={{
+          pattern: {
+            prefix: PREFIX,
+            suffix: SUFFIX,
+            seperator: SEPERATOR,
+          },
+          elementTypes: {
+            [ELEMENT_TYPE.BUTTON]: Button,
+            [ELEMENT_TYPE.BOLD]: Bold,
+          },
+        }}
+      >
         <section>
-          <h1>Temp</h1>
-          <Temp2></Temp2>
+          <h1>String Managed By State</h1>
+          <StringManagedByState></StringManagedByState>
         </section>
-        <p>{`corr<<!bold!{"data":{"text":"ect and wor"}}!>>king corr<<!bold!'{"data":{"name":"ect and wor"}}'!>>king corr<<!bold!'{"data":{"name":"ect and wor"}}'!>>king`}</p>
-        <h1>String string</h1>
-        <p>{`Hello Taranjeet Singh ${PREFIX}${SEPERATOR}${ELEMENT_TYPE.BUTTON}${SEPERATOR}'{"data":{"text":"Test Button"}}'${SEPERATOR}${SUFFIX} How are you`}</p>
-      </div>
-      <div>
-        <h1>Multiple string</h1>
-        <p>{`Hello Taranjeet Singh ${PREFIX}${SEPERATOR}${ELEMENT_TYPE.BOLD}${SEPERATOR}'{"data":{"text":"Test Button"}}'${SEPERATOR}${SUFFIX} How are you`}</p>
-        <Strong></Strong>
-        <p>{`Hello Taranjeet Singh ${PREFIX}${SEPERATOR}${ELEMENT_TYPE.BUTTON}${SEPERATOR}'{"data":{"text":"Test Button"}}'${SEPERATOR}${SUFFIX} How are you`}</p>
-        <p
-          style={{ backgroundColor: 'red' }}
-        >{`Hello Taranjeet Singh <<!bold!'{"data":{"text":"Test Button"}}'!>> How are you Hello Taranjeet Singh <<!bold!'{"data":{"text":"Test Button"}}'!>> How are youHello Taranjeet Singh <<!bold!'{"data":{"text":"Test Button"}}'!>> How are youHello Taranjeet Singh <<!bold!'{"data":{"text":"Test Button"}}'!>> How are youHello Taranjeet Singh <<!bold!'{"data":{"text":"Test Button"}}'!>> How are youHello Taranjeet Singh <<!bold!'{"data":{"text":"Test Button"}}'!>> How are youHello Taranjeet Singh <<!bold!'{"data":{"text":"Test Button"}}'!>> How are youHello Taranjeet Singh <<!bold!'{"data":{"text":"Test Button"}}'!>> How are youHello Taranjeet Singh <<!bold!'{"data":{"text":"Test Button"}}'!>> How are youHello Taranjeet Singh <<!bold!'{"data":{"text":"Test Button"}}'!>> How are youHello Taranjeet Singh <<!bold!'{"data":{"text":"Test Button"}}'!>> How are youHello Taranjeet Singh <<!bold!'{"data":{"text":"Test Button"}}'!>> How are youHello Taranjeet Singh <<!bold!'{"data":{"text":"Test Button"}}'!>> How are youHello Taranjeet Singh <<!bold!'{"data":{"text":"Test Button"}}'!>> How are youHello Taranjeet Singh <<!bold!'{"data":{"text":"Test Button"}}'!>> How are you`}</p>
-      </div>
-      <div>
-        <h1>Multiple patterns in single string</h1>
-        <p>{`Hello Taranjeet Singh ${PREFIX}${SEPERATOR}${ELEMENT_TYPE.BUTTON}${SEPERATOR}'{"data":{"text":"Test Button"}}'${SEPERATOR}${SUFFIX} How are youHello Taranjeet Singh ${PREFIX}${SEPERATOR}${ELEMENT_TYPE.BUTTON}${SEPERATOR}'{"data":{"text":"Test Button"}}'${SEPERATOR}${SUFFIX} How are youHello Taranjeet Singh ${PREFIX}${SEPERATOR}${ELEMENT_TYPE.BUTTON}${SEPERATOR}'{"data":{"text":"Test Button"}}'${SEPERATOR}${SUFFIX} How are youHello Taranjeet Singh ${PREFIX}${SEPERATOR}${ELEMENT_TYPE.BUTTON}${SEPERATOR}'{"data":{"text":"Test Button"}}'${SEPERATOR}${SUFFIX} How are youHello Taranjeet Singh ${PREFIX}${SEPERATOR}${ELEMENT_TYPE.BUTTON}${SEPERATOR}'{"data":{"text":"Test Button"}}'${SEPERATOR}${SUFFIX} How are youHello Taranjeet Singh ${PREFIX}${SEPERATOR}${ELEMENT_TYPE.BUTTON}${SEPERATOR}'{"data":{"text":"Test Button"}}'${SEPERATOR}${SUFFIX} How are youHello Taranjeet Singh ${PREFIX}${SEPERATOR}${ELEMENT_TYPE.BUTTON}${SEPERATOR}'{"data":{"text":"Test Button"}}'${SEPERATOR}${SUFFIX} How are youHello Taranjeet Singh ${PREFIX}${SEPERATOR}${ELEMENT_TYPE.BUTTON}${SEPERATOR}'{"data":{"text":"Test Button"}}'${SEPERATOR}${SUFFIX} How are youHello Taranjeet Singh ${PREFIX}${SEPERATOR}${ELEMENT_TYPE.BUTTON}${SEPERATOR}'{"data":{"text":"Test Button"}}'${SEPERATOR}${SUFFIX} How are you`}</p>
-      </div>
-      
-    </Replacer>
-    </div>
-  );
-};
 
-ReactDOM.render(<App />, document.getElementById('root'));
+        {/* Multiple patterns in single string */}
+        <section>
+          <h1>Multiple patterns in single string</h1>
+          <p>{`corr<<!bold!{"data":{"text":"ect and wor"}}!>>king corr<<!bold!'{"data":{"text":"ect and wor"}}'!>>king corr<<!bold!'{"data":{"text":"ect and wor"}}'!>>king`}</p>
+        </section>
+
+        {/* Multiple variable strings */}
+        <section>
+          <h1>Multiple variable strings</h1>
+          <p>
+            corr
+            {`${PREFIX}${SEPERATOR}${ELEMENT_TYPE.BOLD}${SEPERATOR}${JSON}${SEPERATOR}${SUFFIX}king corr${PREFIX}${SEPERATOR}${ELEMENT_TYPE.BOLD}${SEPERATOR}${JSON}${SEPERATOR}${SUFFIX}king||corr${PREFIX}${SEPERATOR}${ELEMENT_TYPE.BOLD}${SEPERATOR}${JSON}${SEPERATOR}${SUFFIX}`}
+            king
+          </p>
+          <RandomConcat />
+        </section>
+
+        <section>
+          <h1>Show hide pattern test</h1>
+          <ShowHideString />
+        </section>
+      </Replacer>
+    </div>
+  )
+}
+
+ReactDOM.render(<App />, document.getElementById('root'))
